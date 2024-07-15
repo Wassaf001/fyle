c23c844619f66107ab4ce1fe641dbdb0ff22bdb1
Wassaf Ali <wassafali@Mohammads-MacBook-Air.local>
2024-07-15 23:14:03 +0530

    Second

--- src/app/my_components/graph/graph.component.ts
+++ src/app/my_components/graph/graph.component.ts
@@ -2,17 +2,63 @@ import { Component } from '@angular/core';
 import {MatRippleModule} from '@angular/material/core';
 import {MatTableModule} from '@angular/material/table';
 
-const ELEMENT_DATA = [
-  {name: 'Hydrogen'},
-  {name: 'Helium'},
-  {name: 'Lithium'},
-  {name: 'Beryllium'},
-  {name: 'Boron'},
-  {name: 'Carbon'},
-  {name: 'Nitrogen'},
-  {name: 'Oxygen'},
-  {name: 'Fluorine'},
-  {name: 'Neon'},
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    workouts: [
+      { type: 'Running', minutes: 30 },
+      { type: 'Cycling', minutes: 45 }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    workouts: [
+      { type: 'Running', minutes: 30 },
+      { type: 'Swimming', minutes: 30 }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Mike Johnson',
+    workouts: [
+      { type: 'Yoga', minutes: 50 },
+      { type: 'Cycling', minutes: 40 }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Mike Johnson',
+    workouts: [
+      { type: 'Yoga', minutes: 50 },
+      { type: 'Cycling', minutes: 40 }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Mike Johnson',
+    workouts: [
+      { type: 'Yoga', minutes: 50 },
+      { type: 'Cycling', minutes: 40 }
+    ]
+  },
+  {
+    id: 6,
+    name: 'Mike Johnson',
+    workouts: [
+      { type: 'Yoga', minutes: 50 },
+      { type: 'Cycling', minutes: 40 }
+    ]
+  },
+  {
+    id: 7,
+    name: 'Mike Johnson',
+    workouts: [
+      { type: 'Yoga', minutes: 50 },
+      { type: 'Cycling', minutes: 40 }
+    ]
+  }
 ];
 
 @Component({
@@ -24,5 +70,5 @@ const ELEMENT_DATA = [
 })
 export class GraphComponent {
   displayedColumns: string[] = ['name'];
-  dataSource = ELEMENT_DATA;
+  dataSource = users; 
 }
